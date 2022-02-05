01941df0a3e924bce21e101ed81cbba2518fde63
Thiago Nascimbeni <tnascimbeni@gmail.com>
2022-02-04 22:18:48 -0300

    Initial Commit

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,102 @@
+import { getAuth, updateProfile } from "firebase/auth"
+import { doc, updateDoc } from "firebase/firestore"
+import { db } from "../firebase.config"
+import { useState } from "react"
+import { useNavigate, Link } from "react-router-dom"
+import { toast } from "react-toastify"
+import ArrowRight from "../assets/svg/keyboardArrowRightIcon.svg"
+import HomeIcon from "../assets/svg/homeIcon.svg"
+
+function Profile() {
+  const auth = getAuth()
+  const [changeDetails, setChangeDetails] = useState(false)
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  })
+  const { name, email } = formData
+
+  const navigate = useNavigate()
+
+  const onLogout = () => {
+    auth.signOut()
+    navigate("/")
+  }
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        })
+
+        const userRef = doc(db, "users", auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name,
+        })
+      }
+    } catch (error) {
+      toast.error("Couldn't update profile details")
+    }
+  }
+
+  const onChange = event => {
+    setFormData(prevState => ({
+      ...prevState,
+      [event.target.id]: event.target.value,
+    }))
+  }
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && onSubmit()
+              setChangeDetails(prevState => !prevState)
+            }}
+          >
+            {changeDetails ? "Done" : "Change"}
+          </p>
+        </div>
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+
+        <Link to="/create-listing" className="createListing">
+          <img src={HomeIcon} alt="home" />
+          <p>Sell or Rent your home</p>
+          <img src={ArrowRight} alt="arrow right" />
+        </Link>
+      </main>
+    </div>
+  )
+}
+
+export default Profile
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,117 @@
+import { useState } from "react"
+import { toast } from "react-toastify"
+import { Link, useNavigate } from "react-router-dom"
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth"
+import { setDoc, doc, serverTimestamp } from "firebase/firestore"
+import { db } from "../firebase.config"
+import OAuth from "../components/OAuth"
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg"
+import visibilityIcon from "../assets/svg/visibilityIcon.svg"
+
+function SignUp() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+  const { name, email, password } = formData
+
+  const navigate = useNavigate()
+
+  const onChange = event => {
+    setFormData(prevState => ({
+      ...prevState,
+      [event.target.id]: event.target.value,
+    }))
+  }
+
+  const onSubmit = async event => {
+    event.preventDefault()
+
+    try {
+      const auth = getAuth()
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      )
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
+
+      const dataCopy = { ...formData }
+      delete dataCopy.password
+      dataCopy.timestamp = serverTimestamp()
+
+      await setDoc(doc(db, "users", user.uid), dataCopy)
+
+      navigate("/")
+    } catch (error) {
+      toast.error("Something went wrong, please try again")
+    }
+  }
+
+  return (
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Sign Up</p>
+        </header>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            className="nameInput"
+            id="name"
+            value={name}
+            onChange={onChange}
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            className="emailInput"
+            id="email"
+            value={email}
+            onChange={onChange}
+            placeholder="E-mail"
+          />
+          <div className="passwordInputDiv">
+            <input
+              type={showPassword ? "text" : "password"}
+              className="passwordInput"
+              placeholder="Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+            />
+            <img
+              src={visibilityIcon}
+              alt="Show Password"
+              className="showPassword"
+              onClick={() => setShowPassword(prevState => !prevState)}
+            />
+          </div>
+          <div className="signUpBar">
+            <p className="signUpText">Sign Up</p>
+            <button className="signUpButton">
+              <ArrowRightIcon fill="#ffffff" width="34px" heigth="34px" />
+            </button>
+          </div>
+        </form>
+        <OAuth />
+        <Link to="/sign-in" className="registerLink">
+          Already have an account? Sign In
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default SignUp
